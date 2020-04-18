6c0b9d8f3fc5dbe7ecc48e2c3f0163b94f618d04
Raghava Goteti <raghava.sasikanth@gmail.com>
2020-04-18 16:25:51 +0530

    Refresh data every time site is up and load live data from json directly

--- core/scrap.py
+++ core/scrap.py
@@ -59,45 +59,50 @@ def t_n(x,n=50):
         return (list(x.sort_values(by="Date Announced", ascending=True)["Date Announced"])[n] - datetime.datetime(2020,1,1,0,0,0,0)).days
     else :return len(_arr) -n
 
+#
+# if not (os.path.exists("data/covid.csv") and os.path.exists("data/covid_Series.csv")):
+#     with urllib.request.urlopen(url) as fp:
+#         mybytes = fp.read()
+#         mystr = mybytes.decode("utf8")
+#
+#     os.chdir(pathlib.Path(__file__).parent.parent.absolute())
+#
+#     with open("data/covid.csv", "w", newline='') as outfile:
+#         writer = csv.writer(outfile)
+#
+#         tree = BeautifulSoup(mystr, "lxml")
+#         table_tag = tree.select("table")[0]
+#         tab_data = [[item.text for item in row_data.select("th,td")]
+#                     for row_data in table_tag.select("tr")]
+#         for data in tab_data:
+#             writer.writerow(data)
+#
+#     df = pd.read_csv("data/covid.csv", skiprows=[0]).iloc[:, 1:20]
+#     df = df[df["Date Announced"].notnull()]
+states = pd.read_csv("data/States.csv")
+df =pd.read_json("https://api.covid19india.org/raw_data.json",orient = 'records')
+df = pd.read_json(df["raw_data"].to_json(),orient='index')
+df.rename(columns={"dateannounced":"Date Announced","detectedstate":"Detected State","patientnumber":"Patient Number"},inplace=True)
+df = df[(df["Date Announced"].notnull()) & (df["Date Announced"] != "")]
+df["Date Announced"] = pd.to_datetime(df["Date Announced"], format='%d/%m/%Y')
+df = df.merge(states, how='left', left_on="Detected State", right_on="States")
+t_n_data = df.groupby("States").apply(t_n,(global_dict["I0"])).reset_index().rename({0:"TN"},axis=1)
+states_series = df.groupby(["States", "Latitude", "Longitude", "Date Announced"], as_index=False)[
+    "Patient Number"].count()
+states = states_series.groupby(["States", "Latitude", "Longitude"], as_index=False).apply(properties).reset_index()
+population = pd.read_csv("data/population.csv", usecols=["States", "Population"])
+states = states.merge(population, on="States")
+states["TNaught"] = (states.Reported - datetime.datetime(2020,1,1,0,0,0,0)).dt.days
+states["Population"] = states["Population"].astype(int)
+states = states.merge(t_n_data, on="States")
+states = states[states.TN>0]
+states[states.TN>0].to_csv("data/covid.csv", index=False)
+states_series.to_csv("data/covid_Series.csv", index=False)
 
-if not (os.path.exists("data/covid.csv") and os.path.exists("data/covid_Series.csv")):
-    with urllib.request.urlopen(url) as fp:
-        mybytes = fp.read()
-        mystr = mybytes.decode("utf8")
-
-    os.chdir(pathlib.Path(__file__).parent.parent.absolute())
-
-    with open("data/covid.csv", "w", newline='') as outfile:
-        writer = csv.writer(outfile)
-
-        tree = BeautifulSoup(mystr, "lxml")
-        table_tag = tree.select("table")[0]
-        tab_data = [[item.text for item in row_data.select("th,td")]
-                    for row_data in table_tag.select("tr")]
-        for data in tab_data:
-            writer.writerow(data)
-
-    df = pd.read_csv("data/covid.csv", skiprows=[0]).iloc[:, 1:20]
-    df = df[df["Date Announced"].notnull()]
-    states = pd.read_csv("data/States.csv")
-    df["Date Announced"] = pd.to_datetime(df["Date Announced"], format='%d/%m/%Y')
-    df = df.merge(states, how='left', left_on="Detected State", right_on="States")
-    t_n_data = df.groupby("States").apply(t_n,(global_dict["I0"])).reset_index().rename({0:"TN"},axis=1)
-    states_series = df.groupby(["States", "Latitude", "Longitude", "Date Announced"], as_index=False)[
-        "Patient Number"].count()
-    states = states_series.groupby(["States", "Latitude", "Longitude"], as_index=False).apply(properties).reset_index()
-    population = pd.read_csv("data/population.csv", usecols=["States", "Population"])
-    states = states.merge(population, on="States")
-    states["TNaught"] = (states.Reported - datetime.datetime(2020,1,1,0,0,0,0)).dt.days
-    states["Population"] = states["Population"].astype(int)
-    states = states.merge(t_n_data, on="States")
-    states[states.TN>0].to_csv("data/covid.csv", index=False)
-    states_series.to_csv("data/covid_Series.csv", index=False)
-
-else:
-    states = pd.read_csv("data/covid.csv")
-    states_series = pd.read_csv("data/covid_Series.csv")
-    states["Reported"] = pd.to_datetime(states["Reported"])
+# else:
+#     states = pd.read_csv("data/covid.csv")
+#     states_series = pd.read_csv("data/covid_Series.csv")
+#     states["Reported"] = pd.to_datetime(states["Reported"])
 
 with open('data/nodal.json') as f :
     raw_nodes = json.load(f)
