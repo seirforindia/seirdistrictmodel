3d82e69fdf7d12488af1f153b8941a6710bc9781
avnih <avnih0@gmail.com>
2020-06-16 12:41:48 +0530

    Added vertical bar for today's prediction and details.

--- visuals/vcolumn.py
+++ visuals/vcolumn.py
@@ -46,19 +46,19 @@ map_column = html.Div(id="selectors", children=[
     ]),
     html.Div(html.Img(src=('https://www.thoughtworks.com/imgs/tw-logo.svg'), style={'height':'10%', 'width':'10%'})),
     html.Div(children=[
-        html.P(u"\n<br/>"+' List of Volunteers ',  style={'margin-top': '35px', 'font-size': '14px', 'float':'left'}),
+        html.P(' List of Volunteers ',  style={'margin-top': '35px', 'font-size': '14px', 'float':'left'}),
         # html.B(children=[html.A("Dr.Vivek Raghavan", href="mailto:vivek.raghavan68@gmail.com", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
         # html.B(children=[html.A("Dr.Shyam Sundaram", href="mailto:vivek.raghavan68@gmail.com", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
         # html.B(children=[html.A("Dr.Rohith Gupta", href="mailto:vivek.raghavan68@gmail.com", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
-        html.B(children=[html.A("Arka A K", href="https://github.com/orgs/Covid19-SEIR/people/ArkaAK", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
-        html.B(children=[html.A("Avni Hirpara", href="https://github.com/orgs/Covid19-SEIR/people/Avni-Hirpara", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
-        html.B(children=[html.A("Raghav Sasikanth", href="https://github.com/orgs/Covid19-SEIR/people/JZZQuant", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
-        html.B(children=[html.A("Sathyan Sethumadhavan", href="https://github.com/orgs/Covid19-SEIR/people/satnair", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
-        html.B(children=[html.A("Mahendra Yadav", href="https://github.com/orgs/Covid19-SEIR/people/userimack", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
-        html.B(children=[html.A("Priya", href="https://github.com/orgs/Covid19-SEIR/people/kumawatanupriya", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
-        html.B(children=[html.A("Anirudh Gupta", href="mailto:anirudh.gupta@thoughtworks.com", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
-        html.B(children=[html.A("Bijayini P", href="mailto:bijayinp@thoughtworks.com ", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
-        html.B(children=[html.A("Nevetha Prakash", href="mailto:nevethap@thoughtworks.com ", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
+        # html.B(children=[html.A("Arka A K", href="https://github.com//ArkaAK", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
+        # html.B(children=[html.A("Avni Hirpara", href="https://github.com/Avni-Hirpara", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
+        # html.B(children=[html.A("Raghav Sasikanth", href="https://github.com/JZZQuant", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
+        # html.B(children=[html.A("Sathyan Sethumadhavan", href="https://github.com/satnair", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
+        # html.B(children=[html.A("Mahendra Yadav", href="https://github.com/userimack", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
+        # html.B(children=[html.A("Priya", href="https://github.com/orgs/kumawatanupriya", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
+        # html.B(children=[html.A("Anirudh Gupta", href="mailto:anirudh.gupta@thoughtworks.com", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
+        # html.B(children=[html.A("Bijayini P", href="mailto:bijayinp@thoughtworks.com ", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
+        # html.B(children=[html.A("Nevetha Prakash", href="mailto:nevethap@thoughtworks.com ", style={'margin-top': '35px', 'margin-left': '5px', 'font-size': '14px', 'float':'left'})]),
         ])
     #  html.Div(children=[
     #      html.A("Global Dict", href="/download_global/"),
@@ -110,7 +110,7 @@ def plot_graph(I, R, Severe_H, R_Fatal, rate_frac, date, cumsum, mt, node, test_
     trace1 = go.Scatter(x=T[days+low_offset:days+high_offset], y=active ,name='Active Infectious', text=np.diff(active),
                     marker=dict(color='rgb(253,192,134,0.2)'), hovertemplate=ht_active)
     total=I[days+low_offset:days+high_offset]+R[days+low_offset:days+high_offset]
-    trace2 = go.Scatter(x=T[days+low_offset:days+high_offset], y=total , name='Total Infected', text=total,
+    trace2 = go.Scatter(x=T[days+low_offset:days+high_offset], y=total , name='Predicted Infected', text=total,
                     marker=dict(color='rgb(240,2,127,0.2)'), hovertemplate=ht_active)
 
     severe=Severe_H[days+low_offset:days+high_offset]
@@ -137,11 +137,25 @@ def plot_graph(I, R, Severe_H, R_Fatal, rate_frac, date, cumsum, mt, node, test_
 
     # find infected and Fatal after 15 and 30 days
     all_dates = [i.date() for i in T]
-    dateAfter15days = datetime.date.today()+datetime.timedelta(days=15)
-    dateAfter30days = datetime.date.today()+datetime.timedelta(days=30)
+    today = datetime.date.today()
+    dateAfter15days = today+datetime.timedelta(days=15)
+    dateAfter30days = today+datetime.timedelta(days=30)
+    todayIndex = all_dates.index(today)
     indexAfter15day = all_dates.index(dateAfter15days)
     indexAfter30day = all_dates.index(dateAfter30days)
 
+    textToday =  ["", today.strftime("%d %b")+',<br>Infected : {:,}'.format((I[todayIndex]+R[todayIndex])) + '<br>'\
+                  +'Fatal : {:,}'.format((R_Fatal[todayIndex]))]
+    barAtToday = go.Scatter(y=[0, (max(I[days+low_offset:days+high_offset])+max(R[days+low_offset:days+high_offset]))/1.5],
+                            x=[T[todayIndex], T[todayIndex]],
+                            mode='lines+text',
+                            showlegend=False,
+                            text=textToday,
+                            textfont_size=12,
+                            line=dict(dash='dash', width=1,color='black'),
+                            textposition="top left",hoverinfo="none")
+    data.append(barAtToday)
+    
     textAt15day =  ["", dateAfter15days.strftime("%d %b")+',<br>Infected : {:,}'.format((I[indexAfter15day]+R[indexAfter15day])) + '<br>'\
                   +'Fatal : {:,}'.format((R_Fatal[indexAfter15day]))]
     barAt15day = go.Scatter(y=[0, (max(I[days+low_offset:days+high_offset])+max(R[days+low_offset:days+high_offset]))/1.5],
