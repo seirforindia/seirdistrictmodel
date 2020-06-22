e8b7014c572f6c082d4a6fb499bbf22af3780948
sathyan sethumadhavan <sathyan.s.nair@gmail.com>
2020-06-22 20:22:03 +0530

    Updated the google analytics

--- assets/gtag.js
+++ assets/gtag.js
@@ -2,4 +2,4 @@ window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-131327483-1');
+  gtag('config', 'UA-169365903-1');
